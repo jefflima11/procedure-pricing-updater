d23f73dbd2f41138a50e34b2db8448595764e1fb
Jefferson Lima Gonçalves <46583320+jefflima11@users.noreply.github.com>
2025-07-02 16:21:00 -0300

    Add menu loop and submenu for option 2 in main.py
    Refactored the main menu to use a while loop, allowing repeated selection of functions without restarting the script. Added a submenu for option 2 to prompt the user to return to the main menu or stay, improving user experience and navigation.

--- main.py
+++ main.py
@@ -8,19 +8,40 @@ import sys
 import os
 
 # print()
-selectOptions = int(input('\n Selecionar função: \n\n 1 - Tratamento de planilha e insercao na tabela de-para. \n 2 - Exibir De-Para de valores externos com internos. \n 3 - Update de valores internos. \n\n 0 - Fechar sistema \n\n'))
 
-if selectOptions == 1:
-    realizar_insert()
-elif selectOptions == 2:
-    print_df = dataframe_export()
-    print(print_df)
-elif selectOptions == 3: 
-    execute_update()
-elif selectOptions == 0:
-    os.system('cls')
-    sys.exit()
-else:
-    print('Opção invalida')
-    sys.exit()
+contador = 0
 
+while contador == 0:
+    selectOptions = int(input('\n Selecionar função: \n\n 1 - Tratamento de planilha e insercao na tabela de-para. \n 2 - Exibir De-Para de valores externos com internos. \n 3 - Update de valores internos. \n\n 0 - Fechar sistema \n\n'))
+    
+    if selectOptions == 1:
+        realizar_insert()
+    elif selectOptions == 2:
+        os.system('cls')
+        print_df = dataframe_export()
+        print(print_df)
+        contador_i = 0
+
+        while contador_i == 0:
+            return_i = input('\n\n Retornar ao menu? ')
+            if return_i == 's':
+                contador_i = 1
+                os.system('cls')
+            elif return_i == 'n':
+                os.system('cls')
+                contador_i = 0
+            else:
+                os.system('cls')
+                print(print_df)
+                print('\n Opção invalida!')
+        
+
+    elif selectOptions == 3: 
+        execute_update()
+    elif selectOptions == 0:
+        os.system('cls')
+        sys.exit()
+        contador+=1
+    else:
+        print('Opção invalida')
+        sys.exit()
