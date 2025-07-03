e975d79feb48d3e6643b5ea5b71403fd16fead94
Jefferson Lima Gonçalves <46583320+jefflima11@users.noreply.github.com>
2025-07-02 22:59:58 -0300

    Refactor DB connection and improve resource cleanup
    Updated Oracle client initialization to use an environment variable for the library directory. Added explicit closing of cursor and connection in dataframe_export to ensure proper resource management. Also, added a print statement to test the connection in connection.py.

--- src/services/from_to_last_value.py
+++ src/services/from_to_last_value.py
@@ -1,41 +1,39 @@
 from src.db.connection import get_connection
 import pandas as pd
-import oracledb
 
 def dataframe_export():
   connect = get_connection()
   cursor = connect.cursor()
-  df = pd.DataFrame()
 
   sql = """
-      SELECT
-    DP.CD_TISS,
-    IB.CD_PRO_FAT,
-    DP.VL_TOTAL NEW_VALUE,
-    VP_ULT.DT_VIGENCIA OLD_DATE,
-    VP_ULT.VL_TOTAL OLD_VALUE
-  FROM
-    DBAHUMS.DE_PARA_HUMS DP
-    INNER JOIN DBAMV.IMP_BRA IB ON DP.CD_TISS = IB.CD_TISS
-    INNER JOIN (
-          SELECT
-            DT_VIGENCIA,
-            CD_PRO_FAT,
-            VL_TOTAL
-          FROM (
+    SELECT
+      DP.CD_TISS,
+      IB.CD_PRO_FAT,
+      DP.VL_TOTAL NEW_VALUE,
+      VP_ULT.DT_VIGENCIA OLD_DATE,
+      VP_ULT.VL_TOTAL OLD_VALUE
+    FROM
+      DBAHUMS.DE_PARA_HUMS DP
+      INNER JOIN DBAMV.IMP_BRA IB ON DP.CD_TISS = IB.CD_TISS
+      INNER JOIN (
             SELECT
-              dt_vigencia,
+              DT_VIGENCIA,
               CD_PRO_FAT,
-              VL_TOTAL,
-              ROW_NUMBER() OVER (PARTITION BY cd_pro_fat ORDER BY DT_VIGENCIA DESC) rn
-            FROM
-              DBAMV.VAL_PRO VP
+              VL_TOTAL
+            FROM (
+              SELECT
+                dt_vigencia,
+                CD_PRO_FAT,
+                VL_TOTAL,
+                ROW_NUMBER() OVER (PARTITION BY cd_pro_fat ORDER BY DT_VIGENCIA DESC) rn
+              FROM
+                DBAMV.VAL_PRO VP
+              WHERE
+                CD_TAB_FAT = 1)
             WHERE
-              CD_TAB_FAT = 1)
-          WHERE
-            RN = 1) VP_ULT ON IB.CD_PRO_FAT = VP_ULT.CD_PRO_FAT
-  WHERE
-    IB.CD_TAB_FAT = 1
+              RN = 1) VP_ULT ON IB.CD_PRO_FAT = VP_ULT.CD_PRO_FAT
+    WHERE
+      IB.CD_TAB_FAT = 1
   """
 
   cursor.execute(sql)
@@ -46,5 +44,7 @@ def dataframe_export():
   cursor = connect.cursor()
 
   df = pd.DataFrame(rows, columns=columns)
+  cursor.close()
+  connect.close()
   return df
 
