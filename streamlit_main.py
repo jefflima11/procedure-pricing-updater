71975f4daea1d607b4d28133a37f213039fe086f
Jefferson Lima Gonçalves <46583320+jefflima11@users.noreply.github.com>
2025-07-17 17:47:26 -0300

    Refactor Streamlit app and add dark theme config
    Added a .streamlit/config.toml file to set the app theme to dark mode. Refactored streamlit_main.py to include direct Oracle DB connection handling, dotenv loading, and improved session/user management. Enhanced the UI with a sidebar, home page, and modularized menu navigation. Added placeholders for data insertion and update functionalities.

--- streamlit_main.py
+++ streamlit_main.py
@@ -1,7 +1,23 @@
 import streamlit as st
-from src.db.connection import get_connection
+import pandas as pd
+import dotenv
+import oracledb
+import os
+from src.services import data_processor
+
+dotenv.load_dotenv()
+
+oracledb.init_oracle_client(lib_dir=os.getenv(""))
 
 def login_page():
+    def get_connection(user, pw, dsn):
+        try:
+            conn = oracledb.connect(user=user, password=pw, dsn=dsn)
+            return conn
+        except oracledb.Error as e:
+            st.error(f"Erro ao conectar ao banco de dados: {e}")
+            return None
+
     st.title("Login")
 
     if "user" not in st.session_state:
@@ -16,29 +32,65 @@ def login_page():
                     st.error("Preencha todos os campos!")
                 else:
                     # Aqui você pode validar no banco se quiser
-                    st.session_state.user = user
-                    st.session_state.pw = pw
-                    st.session_state.dsn = dsn
-                    st.success("Login realizado com sucesso!")
-                    st.rerun()
+                    conn = get_connection(user, pw, dsn)
+                    if conn:
+                        st.session_state.user = user
+                        st.rerun()
 
-    else:
-        st.success(f"Bem-vindo, {st.session_state.user}!")
+def executeInsert():
+    # uploaded_file = st.file_uploader("Envie um documento", type=["csv", "xlsx"])
+    # if uploaded_file is not None:
+    #     df = pd.read_excel(uploaded_file) if uploaded_file.name.endswith('.xlsx') else pd.read_csv(uploaded_file)
+    #     st.write("Dados carregados:")
+        # st.write(insertFromToMatSQL)
+    st.write("Tratamento de dados de medicamentos iniciado...a", data_processor.executeInsert())
+
 
 def menu_page():
-    st.title("Menu Principal")
+    def title(text):
+        st.title(text)
 
-    escolha = st.selectbox("Selecione a função:", ["Inserir", "Atualizar", "Sair"])
+    def home_page():
+        st.markdown(f"""
+            # 🏥 HospUpdate
+            ######
+
+            Bem-vindo ao **HospUpdate**, seu sistema de atualização centralizada de informações hospitalares.
+
+            Você está conectado como **`{st.session_state.user}`**.
+
+            Este sistema permite:
+
+            - 🔄 Atualizar tabelas de procedimentos, valores e vigências
+            - 🧩 Integrar dados diretamente com o banco Oracle
+            - 📁 Gerenciar atualizações com rastreabilidade por usuário
+
+            Use o menu lateral para navegar pelas funcionalidades disponíveis.
+
+            ---
+        """)
+
+    st.sidebar.title(f" {st.session_state.user}")
+    st.sidebar.write("---")
+
+    escolha = st.sidebar.selectbox("Selecione a função:", ["Inserir", "Atualizar", "",], index=2)
 
     if escolha == "Inserir":
+        title("Inserir Dados")
         st.write("Chamaria função de Inserção aqui.")
-        # executeInsert()
+        executeInsert()
     elif escolha == "Atualizar":
+        title("Atualizar Dados")
         st.write("Chamaria função de Atualização aqui.")
-    elif escolha == "Sair":
-        for key in list(st.session_state.keys()):
-            del st.session_state[key]
+    else:
+        home_page()
+
+    st.sidebar.write("---")
+    
+    if st.sidebar.button("Sair"):
+        st.session_state.clear()
         st.rerun()
+
 
 def main():
     st.set_page_config(page_title="Sistema de Atualização", layout="wide")
